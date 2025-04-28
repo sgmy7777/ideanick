151c5aa81f57d98cb3332ec795cfe834d18651b0
Kyart Petr <Kyart.petr@mail.ru>
2025-04-28 16:47:45 +0300

    Add new page for creating NEWIDEA

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { TrpcProvider } from './lib/trpc';
 import { AllIdeasPage } from './pages/AllIdeasPage';
 import { ViewIdeaPage } from './pages/ViewIdeaPage';
-import { getAllIdeasRoute, getViewIdeaRoute, viewIdeaRouteParams } from './lib/routes';
+import * as routes from './lib/routes';
 import { Layout } from './components/Layout';
 import './styles/global.scss';
+import { NewIdeaPage } from './pages/NewIdeaPage';
 
 export const App = () => {
   return (
@@ -12,8 +13,9 @@ export const App = () => {
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
-            <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
-            <Route path={getViewIdeaRoute(viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+            <Route path={routes.getAllIdeasRoute()} element={<AllIdeasPage />} />
+            <Route path={routes.getNewIdeaRoute()} element={<NewIdeaPage />} />
+            <Route path={routes.getViewIdeaRoute(routes.viewIdeaRouteParams)} element={<ViewIdeaPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
