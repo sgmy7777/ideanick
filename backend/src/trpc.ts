2d75a2e8440b01991f81aee03a5d3b32cad52a07
Kyart Petr <Kyart.petr@mail.ru>
2025-04-28 10:53:21 +0300

    Setup ZOD for validation data

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,5 +1,6 @@
 import { initTRPC } from '@trpc/server';
 import _ from 'lodash';
+import { z } from 'zod';
 
 const ideas = _.times(100, (i) => ({
   nick: `cool-idea-nick-${i}`,
@@ -7,19 +8,22 @@ const ideas = _.times(100, (i) => ({
   description: `Descriptions for Idea ${i}...`,
   text: _.times(100, (j) => `<p>Text paragrph ${j} of idea ${i}...</p>`).join(''),
 }));
-// const ideas = [
-//   { nick: 'cool-idea-nick-1', name: 'Idea 1', description: 'Descriptions for Idea 1' },
-//   { nick: 'cool-idea-nick-2', name: 'Idea 2', description: 'Descriptions for Idea 2' },
-//   { nick: 'cool-idea-nick-3', name: 'Idea 3', description: 'Descriptions for Idea 3' },
-//   { nick: 'cool-idea-nick-4', name: 'Idea 4', description: 'Descriptions for Idea 4' },
-//   { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Descriptions for Idea 5' },
-// ];
 
 const trpc = initTRPC.create();
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
     return { ideas: ideas.map((idea) => _.pick(idea, ['nick', 'name', 'description'])) };
   }),
+  getIdea: trpc.procedure
+    .input(
+      z.object({
+        ideaNick: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const idea = ideas.find((idea) => idea.nick === input.ideaNick);
+      return { idea: idea || null };
+    }),
 });
 
 export type TrpcRouter = typeof trpcRouter;
--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,17 +1,31 @@
 import { useParams } from 'react-router-dom';
 import { ViewIdeaRouteParams } from '../../lib/routes';
+import { trpc } from '../../lib/trpc';
 
 export const ViewIdeaPage = () => {
   const { ideaNick } = useParams() as ViewIdeaRouteParams;
+
+  const { data, error, isLoading, isFetching, isError } = trpc.getIdea.useQuery({
+    ideaNick,
+  });
+
+  if (isLoading || isFetching) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  if (!data.idea) {
+    return <span>Idea not found</span>;
+  }
+
   return (
     <div>
-      <h1>{ideaNick}</h1>
-      <p>Description of Idea 1...</p>
-      <div>
-        <p>Text paragraf 1 of idea 1...</p>
-        <p>Text paragraf 2 of idea 1...</p>
-        <p>Text paragraf 3 of idea 1...</p>
-      </div>
+      <h1>{data.idea.name}</h1>
+      <p>{data.idea.description}</p>
+      <div dangerouslySetInnerHTML={{ __html: data.idea.text }} />
     </div>
   );
 };
