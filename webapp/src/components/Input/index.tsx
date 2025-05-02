9c9bc57449ed1162d3508da84f0bb91de73f5beb
Kyart Petr <Kyart.petr@mail.ru>
2025-05-02 14:49:01 +0300

    Improve validations with ZOD

--- webapp/src/components/Input/index.tsx
+++ webapp/src/components/Input/index.tsx
@@ -3,6 +3,7 @@ import { FormikProps } from 'formik';
 export const Input = ({ name, label, formik }: { name: string; label: string; formik: FormikProps<any> }) => {
   const value = formik.values[name];
   const error = formik.errors[name] as string | undefined;
+  const touched = formik.touched[name];
   return (
     <div style={{ marginBottom: 10 }}>
       <label htmlFor={name}>{label}</label>
@@ -12,11 +13,14 @@ export const Input = ({ name, label, formik }: { name: string; label: string; fo
         onChange={(e) => {
           void formik.setFieldValue(name, e.target.value);
         }}
+        onBlur={() => {
+          void formik.setFieldTouched(name);
+        }}
         value={value}
         name={name}
         id={name}
       />
-      {error && <div style={{ color: 'red' }}>{error}</div>}
+      {!!touched && !!error && <div style={{ color: 'red' }}>{error}</div>}
     </div>
   );
 };
