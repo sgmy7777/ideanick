2e9bca72c0aff0a6236481e809644a1edd5d5d8f
Kyart Petr <Kyart.petr@mail.ru>
2025-05-04 20:50:13 +0300

    User registration + endpoint

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -4,6 +4,7 @@ import { trpc } from '../lib/trpc';
 import { createIdeaTrpcRoute } from './createIdea';
 import { getIdeaTrpcRoute } from './getIdea';
 import { getIdeasTrpcRoute } from './getIdeas';
+import { signUpTrpcRoute } from './signUp';
 // @endindex
 
 export const trpcRouter = trpc.router({
@@ -11,6 +12,7 @@ export const trpcRouter = trpc.router({
   createIdea: createIdeaTrpcRoute,
   getIdea: getIdeaTrpcRoute,
   getIdeas: getIdeasTrpcRoute,
+  signUp: signUpTrpcRoute,
   // @endindex
 });
 
