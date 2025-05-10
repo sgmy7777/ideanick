eda9147a6a5610f489fa69634604fa03b26ec94d
Kyart Petr <Kyart.petr@mail.ru>
2025-05-10 13:55:13 +0300

    Prisma: add authorId for Idea

--- backend/src/router/createIdea/index.ts
+++ backend/src/router/createIdea/index.ts
@@ -3,6 +3,9 @@ import { trpc } from '../../lib/trpc';
 import { zCreateIdeaTrpcInput } from './input';
 
 export const createIdeaTrpcRoute = trpc.procedure.input(zCreateIdeaTrpcInput).mutation(async ({ input, ctx }) => {
+  if (!ctx.me) {
+    throw Error('UNAUTHORIZED');
+  }
   const exIdea = await ctx.prisma.idea.findUnique({
     where: {
       nick: input.nick,
@@ -12,7 +15,7 @@ export const createIdeaTrpcRoute = trpc.procedure.input(zCreateIdeaTrpcInput).mu
     throw Error('Idea with this nick already exists');
   }
   await ctx.prisma.idea.create({
-    data: input,
+    data: { ...input, authorId: ctx.me.id },
   });
   return true;
 });
