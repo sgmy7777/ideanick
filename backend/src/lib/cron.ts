67ccd638e61b9ea0926c7b61d2b159c362c6dcdf
Kyart Petr <Kyart.petr@mail.ru>
2025-08-12 15:01:01 +0300

    Make a request that will return the 10 most-liked ideas over the past month

--- backend/src/lib/cron.ts
+++ backend/src/lib/cron.ts
@@ -1,12 +1,14 @@
 import { CronJob } from 'cron';
 
+import { notifyAboutMostLikedIdeas } from '../scripts/notifyAboutMostLikedIdeas';
+
 import { type AppContext } from './ctx';
 
 export const applyCron = (ctx: AppContext) => {
   new CronJob(
     '0 10 1 * *', // At 10:00 on day-of-month 1
     () => {
-      console.info('Hello!');
+      notifyAboutMostLikedIdeas(ctx).catch(console.error);
     },
     null, // onComplete
     true // start right now
