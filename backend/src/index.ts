ddf4028830fb4a25053747b0b07ca2c7368bd280
Kyart Petr <Kyart.petr@mail.ru>
2025-08-12 19:15:07 +0300

    Bring out the 10 most popular ideas in an email using Handlebars loops and helpers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
+// eslint-disable-next-line import/order
+import { env } from './lib/env';
 import cors from 'cors';
 import express from 'express';
 
 import { applyCron } from './lib/cron';
 import { AppContext, createAppContext } from './lib/ctx';
-import { env } from './lib/env';
 import { applyPassportToExpressApp } from './lib/passport';
 import { applyTrpcToExpressApp } from './lib/trpc';
 import { trpcRouter } from './router';
--- backend/src/lib/cron.ts
+++ backend/src/lib/cron.ts
@@ -5,6 +5,7 @@ import { notifyAboutMostLikedIdeas } from '../scripts/notifyAboutMostLikedIdeas'
 import { type AppContext } from './ctx';
 
 export const applyCron = (ctx: AppContext) => {
+  // notifyAboutMostLikedIdeas(ctx).catch(console.error);
   new CronJob(
     '0 10 1 * *', // At 10:00 on day-of-month 1
     () => {
