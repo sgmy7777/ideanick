1a3d6eecb1d37d921a5b325af798bca448cd9a25
Kyart Petr <Kyart.petr@mail.ru>
2025-08-13 19:51:42 +0300

    Log visibility management

--- backend/src/lib/logger.ts
+++ backend/src/lib/logger.ts
@@ -1,5 +1,6 @@
 import { EOL } from 'os';
 
+import debug from 'debug';
 import _ from 'lodash';
 import pc from 'picocolors';
 import { serializeError } from 'serialize-error';
@@ -64,9 +65,15 @@ export const winstonLogger = winston.createLogger({
 
 export const logger = {
   info: (logType: string, message: string, meta?: Record<string, any>) => {
+    if (!debug.enabled(`ideanick:${logType}`)) {
+      return;
+    }
     winstonLogger.info(message, { logType, ...meta });
   },
   error: (logType: string, error: any, meta?: Record<string, any>) => {
+    if (!debug.enabled(`ideanick:${logType}`)) {
+      return;
+    }
     const serializedError = serializeError(error);
     winstonLogger.error(serializedError.message || 'Unknown error', {
       logType,
