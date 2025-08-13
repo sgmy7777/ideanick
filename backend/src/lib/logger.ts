d73bd18c78351cb74e14a4c537cbf5b1844ef796
Kyart Petr <Kyart.petr@mail.ru>
2025-08-13 16:24:58 +0300

    Logs are displayed in a nice yaml format with different colors

--- backend/src/lib/logger.ts
+++ backend/src/lib/logger.ts
@@ -1,5 +1,11 @@
+import { EOL } from 'os';
+
+import _ from 'lodash';
+import pc from 'picocolors';
 import { serializeError } from 'serialize-error';
+import { MESSAGE } from 'triple-beam';
 import winston from 'winston';
+import * as yaml from 'yaml';
 
 import { env } from './env';
 
@@ -15,7 +21,43 @@ export const winstonLogger = winston.createLogger({
   defaultMeta: { service: 'backend', hostEnv: env.HOST_ENV },
   transports: [
     new winston.transports.Console({
-      format: winston.format.json(),
+      format:
+        env.HOST_ENV !== 'local'
+          ? winston.format.json()
+          : winston.format((logData) => {
+              const setColor = {
+                info: (str: string) => pc.blue(str),
+                error: (str: string) => pc.red(str),
+                debug: (str: string) => pc.cyan(str),
+              }[logData.level as 'info' | 'error' | 'debug'];
+              const levelAndType = `${logData.level} ${logData.logType}`;
+              const topMessage = `${setColor(levelAndType)} ${pc.green(logData.timestamp as string)}${EOL}${
+                logData.message
+              }`;
+
+              const visibleMessageTags = _.omit(logData, [
+                'level',
+                'logType',
+                'timestamp',
+                'message',
+                'service',
+                'hostEnv',
+              ]);
+
+              const stringifyedLogData = _.trim(
+                yaml.stringify(visibleMessageTags, (_k, v) => (_.isFunction(v) ? 'Function' : v))
+              );
+
+              const resultLogData = {
+                ...logData,
+                [MESSAGE]:
+                  [topMessage, Object.keys(visibleMessageTags).length > 0 ? `${EOL}${stringifyedLogData}` : '']
+                    .filter(Boolean)
+                    .join('') + EOL,
+              };
+
+              return resultLogData;
+            })(),
     }),
   ],
 });
