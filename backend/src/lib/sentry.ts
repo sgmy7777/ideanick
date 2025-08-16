bb61e36201355abebee1071fd81209016f7f5027
Kyart Petr <Kyart.petr@mail.ru>
2025-08-16 13:05:31 +0300

    Sentry:backend sourcemap

--- backend/src/lib/sentry.ts
+++ backend/src/lib/sentry.ts
@@ -1,3 +1,6 @@
+import path from 'path';
+
+import { RewriteFrames } from '@sentry/integrations';
 import * as Sentry from '@sentry/node';
 
 import { env } from './env';
@@ -9,6 +12,11 @@ if (env.BACKEND_SENTRY_DSN) {
     environment: env.HOST_ENV,
     release: env.SOURCE_VERSION,
     normalizeDepth: 10,
+    integrations: [
+      new RewriteFrames({
+        root: path.resolve(__dirname, '../../..'),
+      }),
+    ],
   });
 }
 
